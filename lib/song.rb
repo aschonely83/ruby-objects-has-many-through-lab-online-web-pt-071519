185c4a7087685d3dac6a2f1e1b109e003ecd8241
Adam Schonely <aschonely@gmail.com>
2019-08-20 11:07:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,16 @@ class Song
   attr_reader :artist, :genre 
   attr_accessor :name 
   
+  @@all = []
+  
   def initialize(name)
     @name = name
     @artist = artist
     @genre = genre
+  end
+  
+  def self.all
+    @@all
   end  
   
 end  
