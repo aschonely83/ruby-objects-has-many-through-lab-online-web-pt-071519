e805d010a41a8c77900b5de90bcc21e7df20f30e
Adam Schonely <aschonely@gmail.com>
2019-08-20 11:12:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,25 @@
-class Artist 
+class Artist
+  attr_accessor :name
 
-  attr_reader :name 
-  
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def new_song(name, genre)
     Song.new(name, self, genre)
-  end  
-  
+  end
+
   def songs
     Song.all.select { |song| song.artist == self }
   end
 
-  def genres 
+  def genres
     songs.map(&:genre)
   end
-end
- 
-  
